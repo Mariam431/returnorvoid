264dc719e763103f7ba4a58649ac5370289e9cbf
mariam <mariam@MYPROBOOK>
2023-03-22 00:24:35 +0400

    Add project files.

---
+++ returnorvoid/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace returnorvoid
+{
+    internal class Class1
+    {
+        public bool Meth (int a, int b)
+        {
+            int t = a + b;
+            return true;
+        }
+        public int Meth1(int a, int b)
+        {
+            int t = a + b;
+            return t;
+        }
+
+
+
+    }
+}
---
+++ returnorvoid/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace returnorvoid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.Meth(5, 7));
+            Console.WriteLine(class1.Meth1(5, 7));
+        }
+    }
+}
